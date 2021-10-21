71d5394e260427bc443bf4bb9d7ee769e95c719b
ksone02 <eksovus@gmail.com>
2021-10-22 02:52:19 +0900

    fix: change format of function to typescipt

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.css';
-import Header from './components/js/Header';
-import Footer from './components/js/Footer';
-import MainBoard from './components/js/MainBoard';
+import React from "react";
+import "./App.css";
+import Header from "./components/js/Header";
+import Footer from "./components/js/Footer";
+import MainBoard from "./components/js/MainBoard";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      <h2>Front-End 초기 세팅 완료</h2>
+      <h2>Front-End 초기 세팅 완료!</h2>
+      <h3>+ 배포 성공!</h3>
       <MainBoard />
       <Footer />
     </div>
   );
-}
+};
 
 export default App;

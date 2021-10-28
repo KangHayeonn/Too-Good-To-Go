c41d2e3366eff93f11cea5292e4b43164499458d
grand7070 <grand7070@naver.com>
2021-10-28 20:22:21 +0900

    add ShopRepositoryTest

--- Back/src/test/java/com/toogoodtogo/domain/shop/ShopRepositoryTest.java
+++ Back/src/test/java/com/toogoodtogo/domain/shop/ShopRepositoryTest.java
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class ShopRepositoryTest {
     @Autowired
     ShopRepository shopRepository;

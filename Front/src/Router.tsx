1f2131106736d00f5e050c9ca9faa7cc793c293e
ksone02 <eksovus@gmail.com>
2021-11-04 21:40:21 +0900

    router 수정, login, register padding-top, footer 수정

--- Front/src/Router.tsx
+++ Front/src/Router.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/organisms/Header/Header";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Shop from "./pages/Shop/Shop";
 import ShopMenuPage from "./pages/ShopMenu/ShopMenuPage";
+import Footer from "./components/organisms/Footer/Footer";
 
 const Router: React.FC = () => {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Switch>
 				<Route path="/shopmenu" component={ShopMenuPage} />
 				<Route path="/shop" component={Shop} />
@@ -15,6 +18,7 @@ const Router: React.FC = () => {
 				<Route path="/register" component={Register} />
 				<Route render={() => <div>에러페이지</div>} />
 			</Switch>
+			<Footer />
 		</BrowserRouter>
 	);
 };

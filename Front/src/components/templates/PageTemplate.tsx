dd4b23fef86baf20bcfe0b9e137bad43d4a46c20
ksone02 <eksovus@gmail.com>
2021-11-07 14:48:43 +0900

    feat(template) custom footer

--- Front/src/components/templates/PageTemplate.tsx
+++ Front/src/components/templates/PageTemplate.tsx
@@ -20,31 +20,42 @@ const Content = styled.section`
 	width: 100%;
 	box-sizing: border-box;
 	margin: 2rem auto;
+	border-bottom: 1px solid #eee;
 `;
 
 const Footer = styled.footer`
 	margin-top: auto;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	padding-bottom: 80px;
 `;
 
 type Props = {
 	header: React.ReactNode;
 	children: React.ReactNode;
-	footer: React.ReactNode;
+	footer?: React.ReactNode;
+	isFooter: boolean; // footer 사용 안하시려면 isFooter={false}하시고 footer는 안쓰셔도 됩니다.
 };
 
 const PageTemplate: React.FC<Props> = ({
 	header,
 	children,
 	footer,
+	isFooter,
 	...props
 }) => {
 	return (
 		<Wrapper {...props}>
 			<Header>{header}</Header>
 			<Content>{children}</Content>
-			<Footer>{footer}</Footer>
+			{isFooter ? <Footer>{footer}</Footer> : null}
 		</Wrapper>
 	);
+};
+
+PageTemplate.defaultProps = {
+	footer: null,
 };
 
 export default PageTemplate;
--- Front/src/pages/ShopMenu/ShopMenuPage.tsx
+++ Front/src/pages/ShopMenu/ShopMenuPage.tsx
@@ -1,16 +1,44 @@
 import React from "react";
+import { css } from "@emotion/react";
+import { Pagination } from "@mui/material";
+import { RouteComponentProps } from "react-router-dom";
 import PageTemplate from "../../components/templates/PageTemplate";
 import ShopCards from "../../components/organisms/ShopMenu/ShopCards";
 import ShopMenuHeader from "../../components/molecules/ShopMenu/ShopMenuHeader";
 import ShopHeaderMenu from "../../components/organisms/ShopMenu/ShopHeaderMenu";
 
-const ShopMenuPage: React.FC = () => {
+const paginationStyle = css`
+	.css-1pi9rcw-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected:hover {
+		background-color: #54b689;
+	}
+	.css-10w330c-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected {
+		background-color: #54b689;
+		color: #fff;
+	}
+
+	.css-1pi9rcw-MuiButtonBase-root-MuiPaginationItem-root {
+		font-size: 15px;
+		font-family: "Pretendard";
+		font-weight: 500;
+	}
+`;
+
+type matchParams = {
+	menuName: string;
+};
+
+const ShopMenuPage: React.FC<RouteComponentProps<matchParams>> = ({
+	match,
+}) => {
 	return (
 		<PageTemplate
 			header={<ShopHeaderMenu />}
-			footer={<div>페이지네이션</div>}
+			footer={
+				<Pagination css={paginationStyle} count={1} shape="rounded" />
+			}
+			isFooter
 		>
-			<ShopMenuHeader>한식</ShopMenuHeader>
+			<ShopMenuHeader>{match.params.menuName}</ShopMenuHeader>
 			<ShopCards />
 		</PageTemplate>
 	);

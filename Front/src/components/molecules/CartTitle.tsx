b3d0a9f1156620fbc8f6e19be6d24b769866b347
akdlsz21 <akdlsz21@gmail.com>
2021-11-10 02:54:20 +0900

    added breadcrumb and cartTitle

---
+++ Front/src/components/molecules/CartTitle.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "@emotion/styled";
+import CartBreadcrumb from "../atoms/Breadcrumb/CartBreadcrumb";
+
+const CartTitle = () => {
+	return (
+		<TitleAndBreadcrumb>
+			<CartText>장바구니</CartText>
+			<CartBreadcrumb />
+		</TitleAndBreadcrumb>
+	);
+};
+
+const TitleAndBreadcrumb = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const CartText = styled.div`
+	width: 158px;
+	height: 65px;
+	font-size: 40px;
+	font-weight: bold;
+	/* flex-direction: row; */
+`;
+
+export default CartTitle;
--- Front/src/pages/Cart/Cart.tsx
+++ Front/src/pages/Cart/Cart.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import styled from "@emotion/styled";
 import PageTemplate from "../../components/templates/PageTemplate";
 import ShopHeaderMenu from "../../components/organisms/ShopMenu/ShopHeaderMenu";
+import CartTitle from "../../components/molecules/CartTitle";
 
 const Cart = () => {
 	return (
 		<PageTemplate header={<ShopHeaderMenu />} isFooter>
-			<Wrapper>d</Wrapper>
+			<Wrapper>
+				<CartTitle />
+			</Wrapper>
 		</PageTemplate>
 	);
 };
@@ -14,6 +17,6 @@ const Cart = () => {
 export default Cart;
 
 const Wrapper = styled.div`
-	width: 100vw;
-	height: 100vh;
+	margin: 0;
+	padding: 0;
 `;

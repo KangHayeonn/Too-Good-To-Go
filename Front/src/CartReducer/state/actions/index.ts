b138aef03d8e69b38c7b4441507908e80163791a
akdlsz21 <81629070+akdlsz21@users.noreply.github.com>
2021-11-12 23:51:31 +0900

    Update index.ts

--- Front/src/CartReducer/state/actions/index.ts
+++ Front/src/CartReducer/state/actions/index.ts
@@ -3,7 +3,7 @@ import { ActionType } from "../action-types";
 // type InitialStateType = ShopsType & {
 // 	isEdit: boolean;
 // };
-
+//
 export type Action = {
 	type: ActionType.CHECK;
 	payload: {

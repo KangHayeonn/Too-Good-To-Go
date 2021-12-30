2564882f1534ba05a0d9c6ee5b28aa63b130acd2
KangHayeonn <rkdgkdus522@naver.com>
2021-12-30 17:22:17 +0900

    Modal Error 수정 후 Upload

--- Front/src/components/atoms/Modal/PaymentModal.tsx
+++ Front/src/components/atoms/Modal/PaymentModal.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState, useCallback, useRef, useEffect} from 'react';
+import React, {useState, useCallback, useRef, useEffect} from 'react';
 import styled from '@emotion/styled';
 
 const Container = styled.div`

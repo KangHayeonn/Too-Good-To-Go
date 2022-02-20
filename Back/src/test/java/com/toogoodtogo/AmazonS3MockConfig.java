24fac0ca148b007748c5c79e6dcde6646dce47eb
grand7070 <grand7070@naver.com>
2022-02-21 04:21:25 +0900

    CI 에러 수정5

--- Back/src/test/java/com/toogoodtogo/AmazonS3MockConfig.java
+++ Back/src/test/java/com/toogoodtogo/AmazonS3MockConfig.java
@@ -7,7 +7,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import io.findify.s3mock.S3Mock;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
